e955537e88ae2c7235e16411602dba7f65210abb
lannisterblr18 <ronjayrudra98@gmail.com>
2025-06-21 07:18:18 +0530

    Syntax changes

--- back-end/src/main/java/com/designflash/demo/DesignFlashApplication.java
+++ back-end/src/main/java/com/designflash/demo/DesignFlashApplication.java
@@ -1,13 +1,22 @@
 package com.designflash.demo;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesignFlashApplication {
 
+	@Value("${server.port}")
+	private String port;
 	public static void main(String[] args) {
 		SpringApplication.run(DesignFlashApplication.class, args);
+	}
+
+	@PostConstruct
+	public void logPort() {
+		System.out.println("🚀 Running on port: " + port);
 	}
 
 }
